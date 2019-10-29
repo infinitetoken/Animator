9e8679deb10fa06b8ca380bdb6979c1b09c5cd2a
Aaron Wright <aaron.c.wright@icloud.com>
2019-10-29 13:57:12 -0500

    Change to milliseconds

--- Sources/Animator.swift
+++ Sources/Animator.swift
@@ -87,7 +87,7 @@ public struct Animator {
                 let frame = frames[frameIndex]
                 
                 if let image = frame.image.centered(in: CGRect(x: 0, y: 0, width: size.width, height: size.height), background: frame.background), let buffer = image.pixelBuffer(size: size) {
-                    adaptor.append(buffer, withPresentationTime: CMTime(seconds: frameTime, preferredTimescale: 1))
+                    adaptor.append(buffer, withPresentationTime: CMTime(seconds: frameTime, preferredTimescale: 1000))
                     
                     frameTime += frame.duration
                 }
