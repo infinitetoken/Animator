6aeaf38a515039bd4dabcd9cff291038080d5547
Aaron Wright <aaron.c.wright@icloud.com>
2019-11-17 05:11:37 -0600

    Rearrange

--- Sources/Animator.swift
+++ Sources/Animator.swift
@@ -37,6 +37,53 @@ public struct Animator {
         var background: CGColor
     }
     
+    public static func animation(from frames: [Frame], outputURL: URL, size: CGSize? = nil, queue: DispatchQueue = DispatchQueue(label: "Animator"), completion: @escaping (Error?) -> Void) {
+        let width = frames.max { (a, b) -> Bool in
+            return a.image.width < b.image.width
+        }?.image.width ?? 0
+        let height = frames.max { (a, b) -> Bool in
+            return a.image.height < b.image.height
+        }?.image.height ?? 0
+        let size = size ?? CGSize(width: width, height: height)
+        
+        guard size.width > 0, size.height > 0 else {
+            completion(AnimatorError.failed)
+            return
+        }
+        
+        queue.async {
+            let fileProperties = [kCGImagePropertyGIFDictionary as String:[
+                kCGImagePropertyGIFLoopCount as String: NSNumber(value: Int32(0) as Int32)],
+                kCGImagePropertyGIFHasGlobalColorMap as String: NSValue(nonretainedObject: true)
+            ] as [String : Any]
+            
+            guard let destination = CGImageDestinationCreateWithURL(outputURL as CFURL, kUTTypeGIF, frames.count, nil) else {
+                DispatchQueue.main.async { completion(AnimatorError.failed) }
+                return
+            }
+            
+            CGImageDestinationSetProperties(destination, fileProperties as CFDictionary)
+            
+            for frame in frames {
+                let frameProperties = [
+                    kCGImagePropertyGIFDictionary as String:[
+                        kCGImagePropertyGIFDelayTime as String: frame.duration
+                    ]
+                ]
+                
+                if let image = frame.image.centered(in: CGRect(x: 0, y: 0, width: size.width, height: size.height), background: frame.background) {
+                    CGImageDestinationAddImage(destination, image, frameProperties as CFDictionary)
+                }
+            }
+            
+            if CGImageDestinationFinalize(destination) {
+                DispatchQueue.main.async { completion(nil) }
+            } else {
+                DispatchQueue.main.async { completion(AnimatorError.failed) }
+            }
+        }
+    }
+    
     public static func movie(from frames: [Frame], outputURL: URL, size: CGSize? = nil, queue: DispatchQueue = DispatchQueue(label: "Animator"), completion: @escaping (Error?) -> Void) {
         var assetWriter: AVAssetWriter
         
@@ -107,53 +154,6 @@ public struct Animator {
                 } else {
                     DispatchQueue.main.async { completion(nil) }
                 }
-            }
-        }
-    }
-    
-    public static func animation(from frames: [Frame], outputURL: URL, size: CGSize? = nil, queue: DispatchQueue = DispatchQueue(label: "Animator"), completion: @escaping (Error?) -> Void) {
-        let width = frames.max { (a, b) -> Bool in
-            return a.image.width < b.image.width
-        }?.image.width ?? 0
-        let height = frames.max { (a, b) -> Bool in
-            return a.image.height < b.image.height
-        }?.image.height ?? 0
-        let size = size ?? CGSize(width: width, height: height)
-        
-        guard size.width > 0, size.height > 0 else {
-            completion(AnimatorError.failed)
-            return
-        }
-        
-        queue.async {
-            let fileProperties = [kCGImagePropertyGIFDictionary as String:[
-                kCGImagePropertyGIFLoopCount as String: NSNumber(value: Int32(0) as Int32)],
-                kCGImagePropertyGIFHasGlobalColorMap as String: NSValue(nonretainedObject: true)
-            ] as [String : Any]
-            
-            guard let destination = CGImageDestinationCreateWithURL(outputURL as CFURL, kUTTypeGIF, frames.count, nil) else {
-                DispatchQueue.main.async { completion(AnimatorError.failed) }
-                return
-            }
-            
-            CGImageDestinationSetProperties(destination, fileProperties as CFDictionary)
-            
-            for frame in frames {
-                let frameProperties = [
-                    kCGImagePropertyGIFDictionary as String:[
-                        kCGImagePropertyGIFDelayTime as String: frame.duration
-                    ]
-                ]
-                
-                if let image = frame.image.centered(in: CGRect(x: 0, y: 0, width: size.width, height: size.height), background: frame.background) {
-                    CGImageDestinationAddImage(destination, image, frameProperties as CFDictionary)
-                }
-            }
-            
-            if CGImageDestinationFinalize(destination) {
-                DispatchQueue.main.async { completion(nil) }
-            } else {
-                DispatchQueue.main.async { completion(AnimatorError.failed) }
             }
         }
     }
